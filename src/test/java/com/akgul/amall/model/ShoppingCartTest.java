fff168a2250bba9d382d788fc568ff872ab18b8b
Bahadır Akın Akgül <akinakgul@gmail.com>
2019-12-08 17:19:57 +0300

    Apply discount tests are added.

--- src/test/java/com/akgul/amall/model/ShoppingCartTest.java
+++ src/test/java/com/akgul/amall/model/ShoppingCartTest.java
@@ -1,6 +1,7 @@
 package com.akgul.amall.model;
 
 
+import com.akgul.amall.enums.DiscountType;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
@@ -39,5 +40,59 @@ public class ShoppingCartTest {
         shoppingCart.addItem(product, 0);
 
         Assert.isTrue(shoppingCart.getItems().size() == 0, "Product should not be added when added quantity is zero");
+    }
+
+    @Test
+    public void shouldApplyTheMaximumAmountOfDiscountToTheCart() {
+        Category category = new Category("TestCategory");
+
+        Campaign biggestAmountCampaign = new Campaign(category, BigDecimal.TEN, 1, DiscountType.AMOUNT);
+        Campaign campaign = new Campaign(category, BigDecimal.ONE, 1, DiscountType.AMOUNT);
+        Campaign zeroCampaign = new Campaign(category, BigDecimal.ZERO, 1, DiscountType.AMOUNT);
+
+        Product product = new Product("TestProduct", BigDecimal.valueOf(100), category);
+        product.setQuantity(10);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addItem(product, 1);
+        shoppingCart.applyDiscounts(biggestAmountCampaign, campaign, zeroCampaign);
+
+        Assert.isTrue(shoppingCart.getCartCampaign().equals(biggestAmountCampaign), "Should select biggest amount campaign");
+    }
+
+    @Test
+    public void shouldApplyTheMaximumAmountOfWhenDifferentDiscountTypesAvailable() {
+        Category category = new Category("TestCategory");
+
+        Campaign amountCampaign = new Campaign(category, BigDecimal.TEN, 1, DiscountType.AMOUNT);
+        Campaign rateCampaign = new Campaign(category, BigDecimal.valueOf(25), 1, DiscountType.RATE);
+        Campaign zeroCampaign = new Campaign(category, BigDecimal.ZERO, 1, DiscountType.AMOUNT);
+
+        Product product = new Product("TestProduct", BigDecimal.valueOf(100), category);
+        product.setQuantity(10);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addItem(product, 1);
+        shoppingCart.applyDiscounts(amountCampaign, rateCampaign, zeroCampaign);
+
+        Assert.isTrue(shoppingCart.getCartCampaign().equals(rateCampaign), "When cart total price 100, 25% discount has bigger amount than 10 amount");
+    }
+
+    @Test
+    public void shouldNotApplyTheCampaignWhenAmountIsZero() {
+        Category category = new Category("TestCategory");
+
+        Campaign zeroCampaign1 = new Campaign(category, BigDecimal.ZERO, 1, DiscountType.AMOUNT);
+        Campaign zeroCampaign2 = new Campaign(category, BigDecimal.ZERO, 1, DiscountType.RATE);
+        Campaign zeroCampaign3 = new Campaign(category, BigDecimal.ZERO, 1, DiscountType.AMOUNT);
+
+        Product product = new Product("TestProduct", BigDecimal.valueOf(100), category);
+        product.setQuantity(10);
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addItem(product, 1);
+        shoppingCart.applyDiscounts(zeroCampaign1, zeroCampaign2, zeroCampaign3);
+
+        Assert.isNull(shoppingCart.getCartCampaign(), "Should not be applied when campaign values are zero.");
     }
 }
