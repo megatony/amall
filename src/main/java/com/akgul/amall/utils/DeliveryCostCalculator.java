aa74a923e98261bbed0204b9093da764034ef2bf
Bahadır Akın Akgül <akinakgul@gmail.com>
2019-12-08 16:47:46 +0300

    Delivery cost calculator for cart method implemented.

--- src/main/java/com/akgul/amall/utils/DeliveryCostCalculator.java
+++ src/main/java/com/akgul/amall/utils/DeliveryCostCalculator.java
@@ -1,10 +1,15 @@
 package com.akgul.amall.utils;
 
+import com.akgul.amall.model.Category;
+import com.akgul.amall.model.Product;
 import com.akgul.amall.model.ShoppingCart;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -19,8 +24,23 @@ public class DeliveryCostCalculator {
     private BigDecimal costPerDelivery;
     private BigDecimal costPerProduct;
     private BigDecimal fixedCost;
+    private long deliveryCount = 0;
+    private long productCount = 0;
 
     protected double calculateFor(ShoppingCart cart) {
-        return 0;
+        HashMap<Product, Long> items = cart.getItems();
+        HashSet<Category> categories = new HashSet<>();
+
+        for (Map.Entry<Product, Long> item : items.entrySet()) {
+            Category productCategory = item.getKey().getCategory();
+            categories.add(productCategory);
+            productCount += item.getValue();
+        }
+
+        deliveryCount = categories.size();
+
+        return costPerDelivery.multiply(BigDecimal.valueOf(deliveryCount))
+                .add(costPerProduct.multiply(BigDecimal.valueOf(productCount)))
+                .add(fixedCost).doubleValue();
     }
 }
