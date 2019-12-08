7bc27bfcb5b5928ab7a926e80389b561741de2d9
Bahadır Akın Akgül <akinakgul@gmail.com>
2019-12-08 03:24:30 +0300

    Initial commit

---
+++ src/main/java/com/akgul/amall/AmallApplication.java
@@ -0,0 +1,13 @@
+package com.akgul.amall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmallApplication.class, args);
+    }
+
+}
