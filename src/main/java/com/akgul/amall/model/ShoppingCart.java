612bed048503e517f134d7d80ba9f1d3e46164eb
Bahadır Akın Akgül <akinakgul@gmail.com>
2019-12-08 15:55:30 +0300

    Model of shopping cart is updated and empty methods are created.

--- src/main/java/com/akgul/amall/model/ShoppingCart.java
+++ src/main/java/com/akgul/amall/model/ShoppingCart.java
@@ -3,12 +3,18 @@ package com.akgul.amall.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 
 @Getter
 @Setter
 public class ShoppingCart extends AmallObject {
     private HashMap<Product, Long> items = new HashMap<>();
+    private Campaign cartCampaign;
+    private Coupon cartCoupon;
+    private BigDecimal totalPrice;
+    private BigDecimal totalDiscount;
+    private BigDecimal deliveryCost;
 
     protected void addItem(Product product, long quantity) {
         if (quantity == 0 || product.getQuantity() < quantity) {
@@ -16,5 +22,29 @@ public class ShoppingCart extends AmallObject {
         }
 
         items.put(product, quantity);
+    }
+
+    protected void applyDiscounts(Campaign discount1, Campaign discount2, Campaign discount3) {
+
+    }
+
+    protected double getTotalAmountAfterDiscounts() {
+        return 0;
+    }
+
+    protected double getCouponDiscount() {
+        return 0;
+    }
+
+    protected double getCampaignDiscount() {
+        return 0;
+    }
+
+    protected double getDeliveryCost() {
+        return 0;
+    }
+
+    protected String print() {
+        return "";
     }
 }
